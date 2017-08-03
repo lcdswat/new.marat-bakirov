da168a778d32ef0c3b421915b68d27115ac81a36
Marat Bakirov <lcdswat@debian-web>
2017-08-03 08:05:11 -0500

    Upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/header.html'); ?>
+	<title>Марат Бакиров</title>
+</head>
+<body>
+<div id="wrapper">
+
+	<!-- <div id="menu"></div> -->
+	
+	<div id="about-me">
+		<img src="/site-files/me-mainpage.svg">
+	</div>
+
+	<div id="fresh">
+		<img src="/i/infobot-macbook.png">
+	</div >
+	
+	<!-- Портфолио -->
+	<div id="portfolio">
+		<h1>Портфолио</h1>
+		<div>
+			<img src="/site-files/fish.svg">
+			Сайт infobot.pro<span>25.07.2017</span>
+		</div>
+		<div>
+			<img src="/site-files/fish.svg">
+			Сайт infobot.pro<span>25.07.2017</span>
+		</div>
+		<div>
+			<img src="/site-files/fish.svg">
+			Сайт infobot.pro<span>25.07.2017</span>
+		</div>
+		<div><img src="/site-files/fish.svg">
+			Сайт infobot.pro<span>25.07.2017</span>
+		</div>
+		<div><img src="/site-files/fish.svg">
+			Сайт infobot.pro<span>25.07.2017</span>
+		</div>
+	</div>
+
+	<!-- Заметки -->
+	<div id="notes-mainpage">
+		<h1>Заметки</h1>
+		<div>
+			<h2>Автоматическое содержание в индизайне</h2>
+			Заметка о том, как создать автоматическое содержание в индизайне и настроить его внешний вид.
+		</div>
+		<div>
+			<h2>Нумерация в индизайне</h2>
+			Сегодня расскажу о том, как сделать нумерацию в индизайне. Эта функция называется «Объединение данных» и принцип её работы заключается в том, что вы можете вставлять в индизайновский документ динамические данные из отдельного файла.
+		</div>
+		<div>
+			<h2>О допечатке</h2>
+			Эта заметка не будет большим и точным обучающим материалом по допечатной подготовке, после изучения которого вы сможете идти работать препрессником. Я просто опишу методы и способы, которыми пользуюсь ежедневно сам.
+		</div>
+		<div>
+			<h2>Инструменты верстальщика</h2>
+			Всем давно известно, что скрипты и плагины для индизайна до смешного облегчают работу верстальщика. А тем, кто ещё не в курсе, пора бы знать. В заметке список и описание скриптов, которые используются в студии каждый день.
+		</div>
+		<a href="/notes">Все заметки</a>
+	</div>
+
+	<!-- Рутина -->
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/routine.html'); ?>
+
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/footer.html'); ?>
+
+</body>
+</html>
