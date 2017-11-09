ac8e35bf08184fcbc7de37a2d7015e269df20545
Marat Bakirov <lcdswat@debian-web>
2017-11-09 07:01:36 -0600

    upload

---
+++ portfolio/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/header.html'); ?>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>Марат Бакиров — Портфолио</title>
+</head>
+<body>
+
+<!-- Меню -->
+<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/menu.html'); ?>
+
+<div id="wrapper">
+
+	<!-- Портфолио -->
+	<div id="portfolio">
+
+		<h1>Портфолио</h1>
+
+		<div>
+			<img src="/site-files/fish.svg">
+			Сайт infobot.pro<span>25.07.2017</span>
+		</div>
+		<div>
+			<img src="/site-files/fish.svg">
+			Сайт infobot.pro<span>25.07.2017</span>
+		</div>
+		<div>
+			<img src="/site-files/fish.svg">
+			Сайт infobot.pro<span>25.07.2017</span>
+		</div>
+		<div><img src="/site-files/fish.svg">
+			Сайт infobot.pro<span>25.07.2017</span>
+		</div>
+		<div><img src="/site-files/fish.svg">
+			Сайт infobot.pro<span>25.07.2017</span>
+		</div>
+	</div>
+
+	<!-- Рутина -->
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/routine.html'); ?>
+
+
+	<!-- Футер -->
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/footer.html'); ?>
+
+</div>
+
+</body>
+</html>
