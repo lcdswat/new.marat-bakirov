434e69de4f90fecd0e8c5a982d2742d9a18664e2
Marat Bakirov <lcdswat@debian-web>
2018-01-09 02:34:34 -0600

    upload

--- index.php
+++ index.php
@@ -19,7 +19,9 @@
 			<p>
 				Душа моя озарена неземной радостью, как эти чудесные весенние утра, которыми я наслаждаюсь от всего сердца. 
 			</p>
-			<a href="/">Посмотреть</a>
+			<a href="/">Посмотреть
+			<svg width="21" height="22" viewBox="0 0 21 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>Rectangle 5</title><desc>Created using Figma</desc><g id="Canvas" transform="translate(-7614 -1133)"><g id="Rectangle 5"><use xlink:href="#path0_stroke" transform="matrix(0.707107 -0.707107 0.707107 0.707107 7614 1143.85)" fill="#FFFFFF"/></g></g><defs><path id="path0_stroke" d="M 13.9288 13.9288L 13.9288 14.4288L 14.4288 14.4288L 14.4288 13.9288L 13.9288 13.9288ZM 13.4288 0L 13.4288 13.9288L 14.4288 13.9288L 14.4288 0L 13.4288 0ZM 13.9288 13.4288L 0 13.4288L 0 14.4288L 13.9288 14.4288L 13.9288 13.4288Z"/></defs></svg
+>			</a>
 		</div>
 	</div >
 	
